190c5b0bca9361a0cd051151bb4e56236371f0b7
Artjom Becker <artjomb.2001@gmail.com>
2026-03-23 01:19:22 +0100

    Regenerated temporary Wrangler build artifacts and updated index.html.

--- .wrangler/tmp/bundle-OxWAuM/middleware-insertion-facade.js
+++ .wrangler/tmp/bundle-OxWAuM/middleware-insertion-facade.js
@@ -1,8 +1,8 @@
-				import worker, * as OTHER_EXPORTS from "/Users/artjombecker/Documents/Arbeit/Kinopolis/kinopolis-automation/.wrangler/tmp/pages-38Uh9g/bundledWorker-0.024582783450830425.mjs";
+				import worker, * as OTHER_EXPORTS from "/Users/artjombecker/Documents/Arbeit/Kinopolis/kinopolis-automation/.wrangler/tmp/pages-s8Pthw/bundledWorker-0.19060898072232746.mjs";
 				import * as __MIDDLEWARE_0__ from "/Users/artjombecker/Documents/Arbeit/Kinopolis/kinopolis-automation/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
 import * as __MIDDLEWARE_1__ from "/Users/artjombecker/Documents/Arbeit/Kinopolis/kinopolis-automation/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
 
-				export * from "/Users/artjombecker/Documents/Arbeit/Kinopolis/kinopolis-automation/.wrangler/tmp/pages-38Uh9g/bundledWorker-0.024582783450830425.mjs";
+				export * from "/Users/artjombecker/Documents/Arbeit/Kinopolis/kinopolis-automation/.wrangler/tmp/pages-s8Pthw/bundledWorker-0.19060898072232746.mjs";
 				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
 				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
 					
