42598e10444ae8066fdfdc69a9120c1d89d4698e
Artjom Becker <artjomb.2001@gmail.com>
2026-04-21 19:00:22 +0200

    fix: remove redundant closing brace in worker script

--- _worker.js
+++ _worker.js
@@ -1140,7 +1140,6 @@ export default {
                                         }, loc);
                                     }
                                 }
-                                }
                             }
                         }
 
