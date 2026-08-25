a7172450e1d189ec29efa83ec80adb46c49981b1
Artjom Becker <artjomb.2001@gmail.com>
2026-08-25 21:11:18 +0200

    feat: migrate project to a native Xcode project structure with a custom SwiftUI tab navigation

---
+++ KinopolisNative/KinopolisNative/KinopolisNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinopolisNativeApp.swift
+//  KinopolisNative
+//
+//  Created by Artjom Becker on 25.08.26.
+//
+
+import SwiftUI
+
+@main
+struct KinopolisNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
